26765270a228e9a154ef1a8bf4806c4fa8e70829
小植同学 <132424145+huangblogs@users.noreply.github.com>
2023-09-05 22:41:30 +0800

    Update config.js

--- public/config.js
+++ public/config.js
@@ -6,10 +6,10 @@ window.Config = {
   // UptimeRobot Api Keys
   // 支持 Monitor-Specific 和 Read-Only
   ApiKeys: [
-    'm784488775-dd1ad84b209c05f8e185c33e',
-    'm784490063-7b5da437e7f1e0d67613714d',
-    'm784497419-de55aa09902ccb3ab22d548a',
-    'm784496436-71a4bf7b1e3bdf7756be131b',
+    'm795224152-7178b731f67f3d83783e3502',
+    'm795224169-beb1ed45c542e10fa84fe892',
+    'm795224320-5abc8d55c166375d6d1be856',
+    'm795224310-cf729d591d97cbf8cd5417c2'
   ],
 
   // 日志天数
